5492f62d9aff23d77dcf15ca4de5d6dcb85561ec
Tudor Pescaru <tudorp00@gmail.com>
2020-04-24 20:12:11 +0300

    Adds homework runner for first homework

---
+++ H1/homework.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import importlib
+
+
+def main():
+    assignments = [file[:-3] for file in os.listdir() if ".py" in file]
+    assignments.remove("homework")
+
+    for assignment in assignments:
+        print(">> Running %s" %(assignment))
+        module = importlib.import_module(assignment)
+        module.main()
+
+    print("All assignments ran")
+
+
+if __name__ == "__main__":
+    main()
+
