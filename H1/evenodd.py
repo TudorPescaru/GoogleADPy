cbe96236b44c04e9b05a663248c928292920079d
Tudor Pescaru <tudorp00@gmail.com>
2020-04-30 20:20:36 +0300

    Updates H1/evenodd.py to accept negative numbers

--- H1/evenodd.py
+++ H1/evenodd.py
@@ -3,7 +3,7 @@
 
 def main():
     x = input('Please input an integer: ')
-    if x.isdigit() is False:
+    if x[1:].isdigit() is False:
         print('That is not a valid input')
     else:
         print('This number is %s' %('even' if int(x) % 2 == 0 else 'odd'))
