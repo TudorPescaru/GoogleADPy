bd3d56d5709ff3ba409f478a3f402d264f748d50
Tudor Pescaru <tudorp00@gmail.com>
2020-04-30 21:24:45 +0300

    Updates homework runner to be more verbose

--- H1/homework.py
+++ H1/homework.py
@@ -6,15 +6,31 @@ import importlib
 
 
 def main():
+    print("## RUNNING HOMEWORK ##")
+    print("==> Getting assignments")
+
     assignments = [file[:-3] for file in os.listdir() if ".py" in file]
     assignments.remove("homework")
 
-    for assignment in assignments:
-        print(">> Running %s" %(assignment))
-        module = importlib.import_module(assignment)
-        module.main()
+    print("==> Assignments found:", assignments)
 
-    print(">> Done")
+    for assignment in assignments:
+        try:
+            print("==> Running '%s'" %(assignment))
+            module = importlib.import_module(assignment)
+            module.main()
+        except KeyboardInterrupt:
+            print("\n==> Aborted")
+            return
+        except Exception as ex:
+            print("==> Error detected")
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            errmesg = template.format(type(ex).__name__, ex.args)
+            print(errmesg)
+            print("==> Skipping assignment")
+            continue
+
+    print("==> Done")
 
 
 if __name__ == "__main__":
